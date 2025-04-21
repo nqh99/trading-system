ef1ce8083152fb3baa4ef28c4ca3299a05853be4
Hugo Nguyen <hugoq.nguyen@gmail.com>
2025-04-21 11:43:30 +0700

    Add validation exception handling

--- src/main/java/org/aqua/trading/controller/GlobalExceptionHandler.java
+++ src/main/java/org/aqua/trading/controller/GlobalExceptionHandler.java
@@ -1,29 +1,73 @@
 package org.aqua.trading.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.aqua.trading.dto.common.ExceptionDto;
 import org.aqua.trading.exception.BusinessException;
+import org.springframework.context.MessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
+  @Override
+  public ResponseEntity<Object> handleHandlerMethodValidationException(
+      HandlerMethodValidationException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    List<Map<String, Object>> detailList =
+        ex.getParameterValidationResults().stream()
+            .map(
+                validationResult -> {
+                  Map<String, Object> detail = new HashMap<>();
+                  detail.put("field", validationResult.getMethodParameter().getParameterName());
+                  detail.put("index", validationResult.getMethodParameter().getParameterIndex());
+
+                  List<String> violations =
+                      validationResult.getResolvableErrors().stream()
+                          .map(MessageSourceResolvable::getDefaultMessage)
+                          .collect(Collectors.toList());
+
+                  detail.put("violations", violations);
+                  return detail;
+                })
+            .toList();
+
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(
+            new ExceptionDto(
+                HttpStatus.BAD_REQUEST.getReasonPhrase(),
+                ((ServletWebRequest) request).getRequest().getServletPath(),
+                "Validation error!",
+                detailList));
+  }
+
   @ExceptionHandler
   public ResponseEntity<ExceptionDto> handleException(
-          Throwable throwable, HttpServletRequest request) {
+      Throwable throwable, HttpServletRequest request) {
+    logError("Unhandled exception", request);
     log.error("Unhandled exception", throwable);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(
-                    new ExceptionDto(
-                            HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
-                            request.getServletPath(),
-                            "Internal server error"));
+        .body(
+            new ExceptionDto(
+                HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase(),
+                request.getServletPath(),
+                "Internal server error"));
   }
 
   @ExceptionHandler(BusinessException.class)
